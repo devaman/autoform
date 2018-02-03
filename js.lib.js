6b4c150955db8cb07e86dd9ce01695da502f7bef
ArpanSamariya <arpansamria16@gmail.com>
2018-02-04 03:12:28 +0530

    Add files via upload

--- js.lib.js
+++ js.lib.js
@@ -4,12 +4,13 @@ let HelloLib=function(params) {
 }
 HelloLib.prototype.init=function () {
     Object.keys(this.params).map(function(inp){
-    	console.log(input(this.params[inp].type,this.params[inp].placeholder))
+        console.log(input(this.params[inp].type,this.params[inp].placeholder))
     }.bind(this))
 }
 function input(type,placeholder){
-	var element=document.createElement("input")
-	element.setAttribute("type",type);
-	element.setAttribute("placeholder",placeholder);
-	return element;
+    var element=document.createElement("input")
+    element.setAttribute("type",type);
+    element.setAttribute("placeholder",placeholder);
+    return element;
 }
+
