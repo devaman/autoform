6a5664239cac6e9b017e7e957b4221c03371c34f
Amit Chambial <chambialamit@gmail.com>
2018-02-04 17:08:29 +0530

    final

---
+++ README.md
@@ -0,0 +1,62 @@
+# AutoForm js
+<img src="https://raw.githubusercontent.com/devaman/autoform/master/images/autoform.png" width="300"/>
+
+[![Codecov](https://img.shields.io/codecov/c/github/codecov/example-python.svg)](#) [![Shippable](https://img.shields.io/shippable/5444c5ecb904a4b21567b0ff.svg)](#)
+[![npm](https://img.shields.io/npm/l/express.svg)](#)
+
+A javascript library to create login ,registration forms and many more types of forms.
+
+## Getting Started
+
+These instructions will get you a copy of the repo on your local machine. See below for notes on how to Contribute.
+
+### Prerequisites
+
+What things you need to write a post.
+- Code Editor
+- Git installed on your computer.
+
+### Usage
+
+A step by step series of examples that tell you how to get this repo in your computer.
+
+### Contributing
+1) Forking the repo.
+2) Cloning the repo. eg
+```bash
+    git clone https://github.com/[your-username]/autoform.git
+```
+3) Creating your own branch.
+```bash
+git checkout -b branch-name
+```
+4) Edit /modify the js.lib.js file
+6) Perform 
+```bash
+git add .
+```
+in your branch to save the current changes.
+
+7) Commit your changes.
+```bash
+git commit -m "some-message"
+``` 
+8) Push Your changes to your Github repo.
+```bash
+git push origin [your-branch-name]
+```
+9) Now go to Github and Send a Pull request to original repo.We will now review your Post and merge with the repo.
+
+## Main Contributors
+
+* [**devaman** ](https://github.com/devaman)
+* [**aryankhandal0** ](https://github.com/aryankhandal0)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](https://github.com/devaman/autoform/blob/master/LICENSE) file for details
+
+## Acknowledgments
+
+* Thanks everyone for your support.
+
---
+++ js.lib.js
@@ -0,0 +1,204 @@
+//code for different functions
+var linebreak = document.createElement("br");
+let HelloLib=function(params) {
+    this.params=params;
+    console.log("Hello World From The library!",this.params)
+}
+HelloLib.prototype.init=function () {
+    Object.keys(this.params).map(function(inp){
+    	if(this.params[inp].type=="inputtext"){
+    		console.log(inputtext(this.params[inp].type,this.params[inp].placeholder,this.params[inp].class,this.params[inp].id));
+    	} else if(this.params[inp].type=="submit"){
+        console.log(insbutton(this.params[inp].value,this.params[inp].class,this.params[inp].id));
+    	} else if(this.params[inp].type=="dropd"){
+        console.log(dropdownfunc(this.params[inp].value,this.params[inp].class,this.params[inp].options,this.params[inp].id))
+      } else if(this.params[inp].type=="textarea"){
+        console.log(textarea(this.params[inp].value,this.params[inp].class,this.params[inp].col,this.params[inp].row,this.params[inp].id));
+      } else if (this.params[inp].type=="cardnum") {
+
+         console.log(credcard(this.params[inp].cardcls,this.params[inp].id,this.params[inp].placeholder));
+         console.log(readOnlyText(this.params[inp].readOnlyText.placeholder,this.params[inp].readOnlyText.class,this.params[inp].readOnlyText.id));
+
+
+      } else if(this.params[inp].type=="phonenum"){
+        console.log(phnum(this.params[inp].class,this.params[inp].id));
+      } else if(this.params[inp].type=="dropd2"){
+        console.log(dropdownfunc2(this.params[inp].class,this.params[inp].options,this.params[inp].id));
+    } else if(this.params[inp].type=="readOnlyText"){
+    }
+    }.bind(this))
+}
+function insbutton(value,clsName,id){
+	var element=document.createElement("FORM");
+  document.getElementsByTagName("BODY").appendChild(linebreak);
+  var data=document.createTextNode(value);
+  element.append(data);
+  element.className += clsName;
+  element.id=id;
+  document.getElementsByTagName("FORM")[0].appendChild(element);
+	return element;
+}
+function inputtext(type,placeholder,clsName,id){
+	var element=document.createElement("INPUT");
+  console.log("linebreak:"+linebreak);
+  document.body.appendChild(linebreak);
+  element.setAttribute("type","text");
+	element.setAttribute("placeholder",placeholder);
+  element.className +="form-control";
+  element.className += clsName;
+  element.setAttribute("id",id);
+  document.getElementsByTagName("FORM")[0].appendChild(element);
+	return element;
+}
+function credcard(clsName,id,ph){
+	var element=document.createElement("INPUT");
+document.getElementsByTagName("FORM")[0].append(linebreak);
+  element.setAttribute("type","text");
+  $(element).inputmask({"mask": "9999 9999 9999 9999"});
+  element.setAttribute("onkeypress","cc_brand_id(event)");
+  element.setAttribute("id","cc");
+  element.setAttribute("placeholder",ph);
+  element.className += clsName;
+  element.setAttribute("id",id);
+	document.getElementsByTagName("FORM")[0].appendChild(element);
+	return element;
+}
+function textarea(value,clsName,col,row,id){
+  var element=document.createElement("TEXTAREA")
+  element.setAttribute('rows',row);
+  element.setAttribute('cols',col);
+  document.getElementsByTagName("FORM")[0].append(linebreak);
+  var data=document.createTextNode(value);
+    element.className +="form-control";
+  //document.getElementsByTagName("BODY")[0].append(element);
+  element.append(data);
+  element.setAttribute("id",id);
+  element.className += clsName;
+  document.getElementsByTagName("FORM")[0].appendChild(element);
+  return element;
+}
+function insbutton(value,clsName,id){
+	var element=document.createElement("BUTTON");
+  document.getElementsByTagName("BODY")[0].append(linebreak);
+  var data=document.createTextNode(value);
+  document.getElementsByTagName("FORM")[0].appendChild(element);
+  element.append(data);
+  element.setAttribute("id",id);
+  element.className += clsName;
+	return element;
+}
+
+  function phnum(clsName,id){
+  	var element=document.createElement("INPUT");
+    document.body.appendChild(linebreak);
+    element.setAttribute("type","text");
+    element.setAttribute("id",id);
+    $(element).inputmask({"mask": "99999-99999"});
+    element.className += clsName;
+    element.className +="form-control";
+    document.getElementsByTagName("FORM")[0].appendChild(element);
+  	return element;
+  }
+function dropdownfunc(value,clsName,options,id){
+	var element=document.createElement("SELECT");
+  document.body.appendChild(linebreak);
+  var data=document.createTextNode(value);
+  console.log(options);
+  element.setAttribute("id",id);
+  element.append(data);
+  element.className += clsName;
+  element.className +="dropdown-menu";
+  Object.keys(options).map(function(opt){
+        var option = document.createElement("option");
+        console.log(options[opt]);
+        option.text=options[opt];
+        option.class="dropdown-item";
+      element.add(option);
+  });
+  element.className += clsName;
+  document.getElementsByTagName("FORM")[0].appendChild(element);
+	return element;
+}
+function readOnlyText(ph,cls,id){
+  console.log("ROT");
+  var element=document.createElement("INPUT");
+  document.body.appendChild(linebreak);
+  element.setAttribute("type","text");
+	element.setAttribute("placeholder",ph);
+  element.className +="form-control";
+  element.className += cls;
+  element.setAttribute("id",id);
+  document.getElementsByTagName("FORM")[0].appendChild(element);
+	return element;
+}
+function dropdownfunc2(clsName,options,id){
+	var element=document.createElement("SELECT");
+  document.body.appendChild(linebreak);
+  //var data=document.createTextNode(value);
+  console.log(options);
+  element.setAttribute("id",id);
+	//document.getElementsByTagName("BODY")[0].append(element);
+
+  //element.append(data);
+  Object.keys(options).map(function(opt){
+        var option = document.createElement("option");
+        console.log(options[opt]);
+        option.text=options[opt];
+      element.add(option);
+  });
+  element.className += clsName;
+    document.getElementsByTagName("FORM")[0].appendChild(element);
+	return element;
+}
+function cc_brand_id(event) {
+  var cur_val = event.target.value;
+  console.log(cur_val);
+  var sel_brand;
+  //var cur_val = $("cc").html;
+  // the regular expressions check for possible matches as you type, hence the OR operators based on the number of chars
+  // regexp string length {0} provided for soonest detection of beginning of the card numbers this way it could be used for BIN CODE detection also
+
+  //JCB
+  jcb_regex = new RegExp('^(?:2131|1800|35)[0-9]{0,}$'); //2131, 1800, 35 (3528-3589)
+  // American Express
+  amex_regex = new RegExp('^3[47][0-9]{0,}$'); //34, 37
+  // Diners Club
+  diners_regex = new RegExp('^3(?:0[0-59]{1}|[689])[0-9]{0,}$'); //300-305, 309, 36, 38-39
+  // Visa
+  visa_regex = new RegExp('^4[0-9]{0,}$'); //4
+  // MasterCard
+  mastercard_regex = new RegExp('^(5[1-5]|222[1-9]|22[3-9]|2[3-6]|27[01]|2720)[0-9]{0,}$'); //2221-2720, 51-55
+  maestro_regex = new RegExp('^(5[06789]|6)[0-9]{0,}$'); //always growing in the range: 60-69, started with / not something else, but starting 5 must be encoded as mastercard anyway
+  //Discover
+  discover_regex = new RegExp('^(6011|65|64[4-9]|62212[6-9]|6221[3-9]|622[2-8]|6229[01]|62292[0-5])[0-9]{0,}$');
+  ////6011, 622126-622925, 644-649, 65
+
+
+  // get rid of anything but numbers
+  cur_val = cur_val.replace(/\D/g, '');
+
+  // checks per each, as their could be multiple hits
+  //fix: ordering matter in detection, otherwise can give false results in rare cases
+  if (cur_val.match(jcb_regex)) {
+    sel_brand = "JCB";
+  } else if (cur_val.match(amex_regex)) {
+    sel_brand = "AMEX";
+  } else if (cur_val.match(diners_regex)) {
+    sel_brand = "Diners Club";
+  } else if (cur_val.match(visa_regex)) {
+    sel_brand = "VISA";
+  } else if (cur_val.match(mastercard_regex)) {
+    sel_brand = "MasterCard";
+  } else if (cur_val.match(discover_regex)) {
+    sel_brand = "Discover";
+  } else if (cur_val.match(maestro_regex)) {
+    if (cur_val[0] == '5') { //started 5 must be mastercard
+      sel_brand = "MasterCard";
+    } else {
+      sel_brand = "Maestro"; //maestro is all 60-69 which is not something else, thats why this condition in the end
+    }
+  } else {
+    sel_brand = "Enter More";
+  }
+  document.getElementById("readOnlyText").value=sel_brand;
+}
