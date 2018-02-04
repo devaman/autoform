5f72dd20d89109e9f15f6921390b8bdb40069660
Aryan Khandal <aryankhandal0@gmail.com>
2018-02-04 17:00:28 +0530

    Update js.lib.js

--- js.lib.js
+++ js.lib.js
@@ -1,5 +1,5 @@
-
-linebreak = document.createElement("br");
+//code for different functions
+var linebreak = document.createElement("br");
 let HelloLib=function(params) {
     this.params=params;
     console.log("Hello World From The library!",this.params)
@@ -7,79 +7,139 @@ let HelloLib=function(params) {
 HelloLib.prototype.init=function () {
     Object.keys(this.params).map(function(inp){
     	if(this.params[inp].type=="inputtext"){
-    		console.log(inputtext(this.params[inp].type,this.params[inp].placeholder,this.params[inp].class));
+    		console.log(inputtext(this.params[inp].type,this.params[inp].placeholder,this.params[inp].class,this.params[inp].id));
     	} else if(this.params[inp].type=="submit"){
-        console.log(insbutton(this.params[inp].value,this.params[inp].class));
+        console.log(insbutton(this.params[inp].value,this.params[inp].class,this.params[inp].id));
     	} else if(this.params[inp].type=="dropd"){
-        console.log(dropdownfunc(this.params[inp].value,this.params[inp].class,this.params[inp].options))
+        console.log(dropdownfunc(this.params[inp].value,this.params[inp].class,this.params[inp].options,this.params[inp].id))
       } else if(this.params[inp].type=="textarea"){
-        console.log(textarea(this.params[inp].value,this.params[inp].class,this.params[inp].col,this.params[inp].row));
+        console.log(textarea(this.params[inp].value,this.params[inp].class,this.params[inp].col,this.params[inp].row,this.params[inp].id));
       } else if (this.params[inp].type=="cardnum") {
-         console.log(credcard(this.params[inp].cardcls))
-      } else if(this.params[inp].type==""){
-        
-      }
+
+         console.log(credcard(this.params[inp].cardcls,this.params[inp].id,this.params[inp].placeholder));
+         console.log(readOnlyText(this.params[inp].readOnlyText.placeholder,this.params[inp].readOnlyText.class,this.params[inp].readOnlyText.id));
+
+
+      } else if(this.params[inp].type=="phonenum"){
+        console.log(phnum(this.params[inp].class,this.params[inp].id));
+      } else if(this.params[inp].type=="dropd2"){
+        console.log(dropdownfunc2(this.params[inp].class,this.params[inp].options,this.params[inp].id));
+    } else if(this.params[inp].type=="readOnlyText"){
+    }
     }.bind(this))
 }
-function insbutton(value,clsName){
-	var element=document.createElement("BUTTON");
-  document.getElementsByTagName("BODY")[0].append(linebreak);
+function insbutton(value,clsName,id){
+	var element=document.createElement("FORM");
+  document.getElementsByTagName("BODY").appendChild(linebreak);
   var data=document.createTextNode(value);
-  document.body.appendChild(element);
   element.append(data);
   element.className += clsName;
+  element.id=id;
+  document.getElementsByTagName("FORM")[0].appendChild(element);
 	return element;
 }
-function inputtext(placeholder,clsName){
+function inputtext(type,placeholder,clsName,id){
 	var element=document.createElement("INPUT");
-  document.getElementsByTagName("BODY")[0].append(linebreak);
-	document.body.append(element);
+  console.log("linebreak:"+linebreak);
+  document.body.appendChild(linebreak);
   element.setAttribute("type","text");
 	element.setAttribute("placeholder",placeholder);
+  element.className +="form-control";
   element.className += clsName;
+  element.setAttribute("id",id);
+  document.getElementsByTagName("FORM")[0].appendChild(element);
 	return element;
 }
-function credcard(clsName){
+function credcard(clsName,id,ph){
 	var element=document.createElement("INPUT");
-  document.body.append(linebreak);
+document.getElementsByTagName("FORM")[0].append(linebreak);
   element.setAttribute("type","text");
-  element.setAttribute("data-inputmask","'mask': '9999 9999 9999 9999'");
+  $(element).inputmask({"mask": "9999 9999 9999 9999"});
   element.setAttribute("onkeypress","cc_brand_id(event)");
   element.setAttribute("id","cc");
+  element.setAttribute("placeholder",ph);
   element.className += clsName;
-	document.body.append(element);
-  $('#cc').inputmask();
+  element.setAttribute("id",id);
+	document.getElementsByTagName("FORM")[0].appendChild(element);
 	return element;
 }
-function textarea(value,clsName,col,row){
+function textarea(value,clsName,col,row,id){
   var element=document.createElement("TEXTAREA")
   element.setAttribute('rows',row);
   element.setAttribute('cols',col);
-  document.getElementsByTagName("BODY")[0].append(linebreak);
+  document.getElementsByTagName("FORM")[0].append(linebreak);
   var data=document.createTextNode(value);
+    element.className +="form-control";
   //document.getElementsByTagName("BODY")[0].append(element);
-  document.body.appendChild(element);
   element.append(data);
+  element.setAttribute("id",id);
   element.className += clsName;
+  document.getElementsByTagName("FORM")[0].appendChild(element);
   return element;
 }
-function insbutton(value,clsName){
+function insbutton(value,clsName,id){
 	var element=document.createElement("BUTTON");
   document.getElementsByTagName("BODY")[0].append(linebreak);
   var data=document.createTextNode(value);
-  document.body.appendChild(element);
+  document.getElementsByTagName("FORM")[0].appendChild(element);
   element.append(data);
+  element.setAttribute("id",id);
   element.className += clsName;
 	return element;
 }
-function dropdownfunc(value,clsName,options){
+
+  function phnum(clsName,id){
+  	var element=document.createElement("INPUT");
+    document.body.appendChild(linebreak);
+    element.setAttribute("type","text");
+    element.setAttribute("id",id);
+    $(element).inputmask({"mask": "99999-99999"});
+    element.className += clsName;
+    element.className +="form-control";
+    document.getElementsByTagName("FORM")[0].appendChild(element);
+  	return element;
+  }
+function dropdownfunc(value,clsName,options,id){
 	var element=document.createElement("SELECT");
   document.body.appendChild(linebreak);
   var data=document.createTextNode(value);
   console.log(options);
-	//document.getElementsByTagName("BODY")[0].append(element);
-  document.body.appendChild(element);
+  element.setAttribute("id",id);
   element.append(data);
+  element.className += clsName;
+  element.className +="dropdown-menu";
+  Object.keys(options).map(function(opt){
+        var option = document.createElement("option");
+        console.log(options[opt]);
+        option.text=options[opt];
+        option.class="dropdown-item";
+      element.add(option);
+  });
+  element.className += clsName;
+  document.getElementsByTagName("FORM")[0].appendChild(element);
+	return element;
+}
+function readOnlyText(ph,cls,id){
+  console.log("ROT");
+  var element=document.createElement("INPUT");
+  document.body.appendChild(linebreak);
+  element.setAttribute("type","text");
+	element.setAttribute("placeholder",ph);
+  element.className +="form-control";
+  element.className += cls;
+  element.setAttribute("id",id);
+  document.getElementsByTagName("FORM")[0].appendChild(element);
+	return element;
+}
+function dropdownfunc2(clsName,options,id){
+	var element=document.createElement("SELECT");
+  document.body.appendChild(linebreak);
+  //var data=document.createTextNode(value);
+  console.log(options);
+  element.setAttribute("id",id);
+	//document.getElementsByTagName("BODY")[0].append(element);
+
+  //element.append(data);
   Object.keys(options).map(function(opt){
         var option = document.createElement("option");
         console.log(options[opt]);
@@ -87,6 +147,7 @@ function dropdownfunc(value,clsName,options){
       element.add(option);
   });
   element.className += clsName;
+    document.getElementsByTagName("FORM")[0].appendChild(element);
 	return element;
 }
 function cc_brand_id(event) {
@@ -119,27 +180,25 @@ function cc_brand_id(event) {
   // checks per each, as their could be multiple hits
   //fix: ordering matter in detection, otherwise can give false results in rare cases
   if (cur_val.match(jcb_regex)) {
-    sel_brand = "jcb";
+    sel_brand = "JCB";
   } else if (cur_val.match(amex_regex)) {
-    sel_brand = "amex";
+    sel_brand = "AMEX";
   } else if (cur_val.match(diners_regex)) {
-    sel_brand = "diners_club";
+    sel_brand = "Diners Club";
   } else if (cur_val.match(visa_regex)) {
-    sel_brand = "visa";
+    sel_brand = "VISA";
   } else if (cur_val.match(mastercard_regex)) {
-    sel_brand = "mastercard";
+    sel_brand = "MasterCard";
   } else if (cur_val.match(discover_regex)) {
-    sel_brand = "discover";
+    sel_brand = "Discover";
   } else if (cur_val.match(maestro_regex)) {
     if (cur_val[0] == '5') { //started 5 must be mastercard
-      sel_brand = "mastercard";
+      sel_brand = "MasterCard";
     } else {
-      sel_brand = "maestro"; //maestro is all 60-69 which is not something else, thats why this condition in the end
+      sel_brand = "Maestro"; //maestro is all 60-69 which is not something else, thats why this condition in the end
     }
   } else {
-    sel_brand = "unknown";
+    sel_brand = "Enter More";
   }
-
-  console.log(sel_brand);
-
+  document.getElementById("readOnlyText").value=sel_brand;
 }
